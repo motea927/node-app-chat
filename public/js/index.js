11dc9b7690c877d5283732026e9a60af3b50d041
motea927 <motea927@yahoo.com.tw>
2018-06-06 09:46:01 +0800

    fix indes.js

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+var socket = io();
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    // socket.emit('createEmail', {
+    //     to: 'jen@example.com',
+    //     text: 'Hey This is Andrew.'
+    // });
+
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log(message);
+});
+// socket.on('newEmail', function(email) {
+//     console.log('New email', email);
+// });
+
+
+
